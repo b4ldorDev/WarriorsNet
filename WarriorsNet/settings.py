740b90c83e448b097b0cdde7620b7dac92506626
b4ldorDev <A01773374@tec.mx>
2025-02-05 22:46:24 -0600

    Preparando para GitHub Pages

--- WarriorsNet/settings.py
+++ WarriorsNet/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for WarriorsNet project.
 
@@ -117,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
