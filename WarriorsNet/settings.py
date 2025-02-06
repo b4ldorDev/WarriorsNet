a0ebc9f5940b72bdaab50bb8fd0a45639dd4d69c
b4ldorDev <A01773374@tec.mx>
2025-02-06 12:05:32 -0600

    render update files

--- WarriorsNet/settings.py
+++ WarriorsNet/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os 
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,8 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u!2$a=09l4kh*i#)n_1t40#&z6-vi8ieup45owen6(hys4oo+h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  True
-ALLOWED_HOSTS = []
+#DEBUG =  True
+DEBUG = False
+ALLOWED_HOSTS = ['*.render.com','WarriorsNet.render.com']
 AUTH_USER_MODEL = 'Registro.Usuario'
 
 # Application definition
@@ -73,13 +75,38 @@ WSGI_APPLICATION = 'WarriorsNet.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost/postgres',
+        conn_max_age=600
+    )
+}
+"""
+
+# Configuración de base de datos
+if 'DATABASE_URL' in os.environ:
+    # Configuración para producción en Render
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL')
+        )
+    }
+else:
+    # Configuración para desarrollo local con SQLite
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
@@ -117,6 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
